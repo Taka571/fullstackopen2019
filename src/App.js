54bef1bc0c74387b61d02c744a81d31c29ea03a4
Takahiro <tk.iyei@gmail.com>
2019-12-21 16:47:11 +0900

    add catch error

--- src/App.js
+++ src/App.js
@@ -42,6 +42,12 @@ const App = (props) => {
       .then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const rows = () => notesToShow.map(note =>
