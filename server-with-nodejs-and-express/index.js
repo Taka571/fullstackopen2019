34823cf8234a139151d58ab852be9844fcb8a4ab
Takahiro <tk.iyei@gmail.com>
2020-01-05 15:20:55 +0900

    change get /api/notes/:id routing

--- server-with-nodejs-and-express/index.js
+++ server-with-nodejs-and-express/index.js
@@ -53,13 +53,9 @@ app.get('/api/notes', (request, response) => {
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  Note.findById(request.params.id).then(note => {
+    response.json(note.toJSON())
+  })
 })
 
 const generateId = () => {
