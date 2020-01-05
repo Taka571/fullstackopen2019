b643e8efdaf1a560847c77faf8df97a1faa136ea
Takahiro <tk.iyei@gmail.com>
2020-01-05 11:28:33 +0900

    change path

--- server-with-nodejs-and-express/index.js
+++ server-with-nodejs-and-express/index.js
@@ -44,11 +44,11 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
-app.get('/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
   if (note) {
@@ -65,7 +65,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/notes', (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
@@ -86,7 +86,7 @@ app.post('/notes', (request, response) => {
   response.json(note)
 })
 
-app.delete('/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
